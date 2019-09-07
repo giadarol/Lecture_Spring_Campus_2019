c2364b9fc419660c339d58924dbf8614610ffc9c
giadarol <giovanni.iadarola@cern.ch>
2019-09-07 15:18:45 +0200

    Struggling with gradient

---
+++ gradient_example/001_plots.py
@@ -0,0 +1,76 @@
+import numpy as np
+from scipy.interpolate import interp2d
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D #Needed to allow projection='3d'
+from matplotlib import cm
+
+import myfilemanager as mfm
+
+ob = mfm.myloadmat_to_obj('twiss_res.mat')
+
+cost_func = np.sqrt((ob.betax_max-200)**2/200**2 + (ob.betax_min-45)**2/45**2)
+
+i_max, j_max = np.unravel_index(np.argmin(cost_func), cost_func.shape)
+
+start_point = (1.6, 1.1)
+
+Gx_map, Gy_map = np.gradient(cost_func)
+
+Gx_map_norm = Gx_map / np.sqrt(Gx_map**2 + Gy_map**2) 
+Gy_map_norm = Gy_map / np.sqrt(Gx_map**2 + Gy_map**2) 
+
+XX, YY = np.meshgrid(ob.str_factor, ob.str_factor)
+cost_interp = interp2d(XX, YY, cost_func.T, kind='linear')
+Gx_interp = interp2d(XX, YY, Gx_map.T, kind='linear')
+Gy_interp = interp2d(XX, YY, Gy_map.T, kind='linear')
+
+gamma = 0.001
+
+N_steps = 1000
+point_list = []
+point = np.array(start_point)
+for ii in range(N_steps):
+    Gx = Gx_interp(point[0], point[1])[0]
+    Gy = Gy_interp(point[0], point[1])[0]
+
+    gx = Gx / np.sqrt(Gx**2 +Gy**2)
+    gy = Gy / np.sqrt(Gx**2 +Gy**2)
+
+    point -= gamma*np.array([gx, gy])
+    point_list.append(point.copy())
+
+point_list = np.array(point_list)
+
+plt.close('all')
+
+fig2 = plt.figure(2)
+ax2 = fig2.add_subplot(1,1,1)
+plt.pcolormesh(ob.str_factor, ob.str_factor, cost_func.T, vmax=1.)
+plt.colorbar()
+ax2.scatter(ob.str_factor[i_max], ob.str_factor[j_max], color='r')
+ax2.scatter(start_point[0], start_point[1], color='y')
+ax2.plot(point_list[:, 0], point_list[:,1], color='k', linewidth=2)
+ax2.quiver(XX.flatten(), YY.flatten(), -100*Gx_map_norm.T.flatten(), -100*Gy_map_norm.T.flatten())
+
+
+fig1 = plt.figure(1, figsize=(1.4*8,1.4*6))
+ax = fig1.gca(projection='3d')
+
+i_cut = 14
+
+cost_func[cost_func>1]=1.
+surf = ax.plot_surface(XX[i_cut:, i_cut:], YY[i_cut:, i_cut:], 
+        cost_func[i_cut:, i_cut:].T, alpha=0.5, #cmap=cm.coolwarm,
+                       linewidth=0, antialiased=True)
+ax.scatter(ob.str_factor[i_max], ob.str_factor[j_max], cost_func[i_max, j_max], color='r')
+ax.set_xlabel('Q1 strength')
+ax.set_ylabel('Q2 strength')
+ax.set_zlabel('Cost function')
+
+fig1.subplots_adjust(bottom=.02, top=.98, left=.02, right=.98)
+ax.azim=59
+ax.elev=16
+
+
+plt.show()
